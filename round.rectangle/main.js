b70e7526892a9763b6db9151fe8f3948cbb2dad3
Sara-Madwar <60344883+Sara-Madwar@users.noreply.github.com>
2020-02-28 14:53:21 +0100

    sara
    new

--- round.rectangle/main.js
+++ round.rectangle/main.js
@@ -1,27 +1,29 @@
+//Set up the position and style of the shape
 var mousePoint = view.center;
-var amount = 25;
+var amount = 35;
 var colors = ['yellow', 'white', 'yellow', 'black'];
 
-for (var i= 0; i < amount; i++) {
-    var rect = new Rectangle([0, 0], [25,25]);
-    rect.center = mousePoint;
-    var path = new Path2D.Rectangle(rect, 6);
-    path.fillColor = color[i % 4];
-    var scale = (1 - i / amount) * 20;
-    path.scale(scale);
+for (var i = 0; i < amount; i++) {
+	var rect = new Rectangle([0, 0], [5, 5]);
+	rect.center = mousePoint;
+	var path = new Path.Rectangle(rect, 6);
+	path.fillColor = colors[i % 4];
+	var scale = (1 - i / amount) * 20;
+	path.scale(scale);
 }
 
+// Handle mouse event
 function onMouseMove(event) {
-    mousePoint = event.point;
+	mousePoint = event.point;
 }
 
 var children = project.activeLayer.children;
 function onFrame(event) {
-    for (var i = 0, l = children.length; i < l; i++){
-        var item = children[i];
-        var delta = (mousePoint - item.position) / (i + 5);
-        item.rotate(Math.sin((event.count + i) / 10) * 7);
-        if (delta.length > 0,1)
-        item.position +=delta;
-    }
+	for (var i = 0, l = children.length; i < l; i++) {
+		var item = children[i];
+		var delta = (mousePoint - item.position) / (i + 5);
+		item.rotate(Math.sin((event.count + i) / 10) * 7);
+		if (delta.length > 0.1)
+			item.position += delta;
+	}
 }
